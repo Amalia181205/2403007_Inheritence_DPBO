0c467c388718a98ee587f30287b6f966926a6af4
Amalia181205 <lutvianiamalia@gmail.com>
2025-10-01 17:39:57 +0700

    Hierarchical

--- hierarchical/Trail.java
+++ hierarchical/Trail.java
@@ -1,3 +1,11 @@
 public class Trail {
-    
+    private String merk;
+
+     public Trail(String merk) {
+        this.merk = merk; 
+    }
+
+    public void melaju(){
+        System.out.println("Trail " + this.merk + " melaju"); 
+    }
 }
